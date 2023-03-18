b2ec0e7f632feaddef5f6fa727ae62683e4cb074
James Tran <jamestran201@github.com>
2023-03-18 09:33:08 -0400

    WIP lab 1: track task state using enum instead of boolean

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -9,35 +9,48 @@ import (
 	"sync"
 )
 
+type taskState int
+
+const (
+	unassigned taskState = iota
+	in_progress
+	completed
+)
+
 type Coordinator struct {
-	mapTasks       map[string]bool
-	reduceTasks    map[int][]string
-	nReduce        int
-	mapTaskLock    sync.Mutex
-	reduceTaskLock sync.Mutex
+	mapTasks map[string]taskState
+	// reduceTasks    map[int][]string
+	nReduce     int
+	mapTaskLock sync.Mutex
+	// reduceTaskLock sync.Mutex
 }
 
 func (c *Coordinator) GetTask(args *GetTaskArgs, reply *GetTaskReply) error {
 	c.mapTaskLock.Lock()
 	defer c.mapTaskLock.Unlock()
 
-	if len(c.mapTasks) != 0 {
-		for filePath, taskAssigned := range c.mapTasks {
-			if taskAssigned {
-				continue
-			}
-
-			c.mapTasks[filePath] = true
-
-			reply.InputFilePaths = []string{filePath}
-			reply.TaskName = "map"
-			reply.NReduceTasks = c.nReduce
-
-			// TODO: Perhaps add a prefix to indicate whether the log message came from the Coordinator or a Worker
-			log.Printf("Assinging map task with input file: %s\n", filePath)
-
-			break
+	performingMapTask := false
+	for filePath, taskState := range c.mapTasks {
+		if taskState != unassigned {
+			continue
 		}
+
+		c.mapTasks[filePath] = in_progress
+
+		reply.InputFilePaths = []string{filePath}
+		reply.TaskName = "map"
+		reply.NReduceTasks = c.nReduce
+
+		performingMapTask = true
+
+		// TODO: Perhaps add a prefix to indicate whether the log message came from the Coordinator or a Worker
+		log.Printf("Assinging map task with input file: %s\n", filePath)
+
+		break
+	}
+
+	if performingMapTask {
+		return nil
 	}
 
 	return nil
@@ -48,7 +61,7 @@ func (c *Coordinator) FinishTask(args *FinishTaskArgs, reply *FinishTaskReply) e
 	defer c.mapTaskLock.Unlock()
 
 	if args.TaskName == "map" {
-		c.mapTasks[args.TaskIdentifier] = true
+		c.mapTasks[args.TaskIdentifier] = completed
 		log.Printf("Marked map task for %s as complete\n", args.TaskIdentifier)
 	}
 
@@ -73,8 +86,8 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 func (c *Coordinator) Done() bool {
 	done := true
-	for _, taskAssigned := range c.mapTasks {
-		done = done && taskAssigned
+	for _, taskState := range c.mapTasks {
+		done = done && (taskState == completed)
 	}
 
 	return done
@@ -85,13 +98,13 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{
-		mapTasks:    map[string]bool{},
-		reduceTasks: map[int][]string{},
-		nReduce:     nReduce,
+		mapTasks: map[string]taskState{},
+		// reduceTasks: []task{},
+		nReduce: nReduce,
 	}
 
 	for _, file := range files {
-		c.mapTasks[file] = false
+		c.mapTasks[file] = unassigned
 	}
 
 	log.Printf("Number of reduce tasks is: %d\n", nReduce)
