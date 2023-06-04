bb91745b50486b7c878ddaf3501f8e7c1c002795
James Tran <jamestran201@github.com>
2023-06-04 16:21:59 -0400

    refactor hearbeat

--- src/raft/append_entries.go
+++ src/raft/append_entries.go
@@ -2,7 +2,6 @@ package raft
 
 import (
 	"fmt"
-	"time"
 )
 
 type AppendEntriesArgs struct {
@@ -83,71 +82,6 @@ func handleAppendEntries(rf *Raft, args *AppendEntriesArgs, reply *AppendEntries
 
 	reply.Term = rf.currentTerm
 	reply.Success = true
-}
-
-func startHeartBeat(rf *Raft) {
-	for i := range rf.peers {
-		if i == rf.me {
-			continue
-		}
-
-		go sendHeartBeatLoop(rf, i)
-	}
-}
-
-func sendHeartBeatLoop(rf *Raft, server int) {
-	var stopCh chan bool
-	for {
-		if stopCh != nil {
-			stopCh <- true
-		}
-
-		stopCh = make(chan bool, 1)
-		go sendHeartBeat(rf, server, stopCh)
-
-		time.Sleep(300 * time.Millisecond)
-	}
-}
-
-func sendHeartBeat(rf *Raft, server int, stopChan chan bool) {
-	if rf.killed() {
-		return
-	}
-
-	shouldExit := false
-	args := &AppendEntriesArgs{}
-	withLock(&rf.mu, func() {
-		if rf.state != leader {
-			shouldExit = true
-
-			debugLog(rf, "Exiting sendHeartBeat routine because server is no longer the leader")
-			return
-		}
-
-		args = &AppendEntriesArgs{
-			Term:         rf.currentTerm,
-			LeaderId:     rf.me,
-			PrevLogIndex: rf.lastLogIndex(),
-			PrevLogTerm:  rf.lastLogTerm(),
-			Entries:      []LogEntry{},
-		}
-	})
-
-	if shouldExit {
-		return
-	}
-
-	debugLog(rf, fmt.Sprintf("Sending heartbeat to %d", server))
-
-	reply := &AppendEntriesReply{}
-	sendAppendEntries(rf, server, args, reply)
-
-	shouldExit = <-stopChan
-	if shouldExit {
-		return
-	}
-
-	handleAppendEntriesResponse(rf, reply)
 }
 
 func sendAppendEntries(rf *Raft, server int, args *AppendEntriesArgs, reply *AppendEntriesReply) {
